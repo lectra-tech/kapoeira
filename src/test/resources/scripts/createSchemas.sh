2e103e3fb29fe5c004e0654535033888817bac5a
Mehdi Rebiai <m.rebiai@lectra.com>
2024-08-07 18:02:30 +0200

    feat: enable basic authent on schema registry for integration tests
    Signed-off-by: Mehdi Rebiai <m.rebiai@lectra.com>

--- src/test/resources/scripts/createSchemas.sh
+++ src/test/resources/scripts/createSchemas.sh
@@ -3,29 +3,35 @@
 curl --request POST \
   --url http://schema-registry:8081/subjects/kapoeira.avrokeyv1/versions \
   --header 'Content-Type: application/vnd.schemaregistry.v1+json' \
+  --user 'srUsername:srPassword' \
   --data @schemas/kapoeira.avrokeyv1.json
 
 curl --request POST \
   --url http://schema-registry:8081/subjects/topic-avrokeyvalue-key/versions \
   --header 'Content-Type: application/vnd.schemaregistry.v1+json' \
+  --user 'srUsername:srPassword' \
   --data @schemas/kapoeira.avrokeyv1.json
 
 curl --request POST \
   --url http://schema-registry:8081/subjects/kapoeira.avrovaluev1/versions \
   --header 'Content-Type: application/vnd.schemaregistry.v1+json' \
+  --user 'srUsername:srPassword' \
   --data @schemas/kapoeira.avrovaluev1.json
 
 curl --request POST \
   --url http://schema-registry:8081/subjects/topic-avrovalue-value/versions \
   --header 'Content-Type: application/vnd.schemaregistry.v1+json' \
+  --user 'srUsername:srPassword' \
   --data @schemas/kapoeira.avrovaluev1.json
 
 curl --request POST \
   --url http://schema-registry:8081/subjects/topic-avrokeyvalue-value/versions \
   --header 'Content-Type: application/vnd.schemaregistry.v1+json' \
+  --user 'srUsername:srPassword' \
   --data @schemas/kapoeira.avrovaluev1.json
 
 curl --request POST \
   --url http://schema-registry:8081/subjects/kapoeira.jsonvaluev1/versions \
   --header 'Content-Type: application/vnd.schemaregistry.v1+json' \
+  --user 'srUsername:srPassword' \
   --data @schemas/kapoeira.jsonvaluev1.json
