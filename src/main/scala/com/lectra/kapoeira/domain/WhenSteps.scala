0a1bd1d0e0a77205e064aa8ca1988e9b157ff4fd
Mehdi Rebiai <m.rebiai@lectra.com>
2025-04-07 11:07:32 +0200

    chore: license 2025
    Signed-off-by: Mehdi Rebiai <m.rebiai@lectra.com>

--- src/main/scala/com/lectra/kapoeira/domain/WhenSteps.scala
+++ src/main/scala/com/lectra/kapoeira/domain/WhenSteps.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 Lectra
+ * Copyright (C) 2025 Lectra
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
